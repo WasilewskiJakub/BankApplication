c986c54c60986774b6aa6ea279631aed5fc7cafd
tbarcinski <tymekbarcinski@gmail.com>
2024-01-29 01:00:01 +0100

    changing layout and setting up initialization for CalculatorPage

--- src/main/java/com/example/bankapplication/controllers/CalculatorExchangePageController.java
+++ src/main/java/com/example/bankapplication/controllers/CalculatorExchangePageController.java
@@ -1,5 +1,6 @@
 package com.example.bankapplication.controllers;
 
+import com.example.bankapplication.controllers.errors.ApiConnectioException;
 import com.example.bankapplication.controllers.helper.SceneSwitcher;
 import com.example.bankapplication.functionalities.charts.CurrencyHolder;
 import com.example.bankapplication.services.CurrencyService;
@@ -7,6 +8,7 @@ import com.example.bankapplication.services.configuration.Currency;
 import com.example.bankapplication.services.configuration.Table;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
@@ -16,10 +18,12 @@ import javafx.scene.text.Text;
 import javafx.util.Pair;
 
 import java.io.IOException;
+import java.net.URL;
 import java.time.LocalDate;
+import java.util.ResourceBundle;
 import java.util.concurrent.ExecutionException;
 
-public class CalculatorExchangePageController {
+public class CalculatorExchangePageController implements Initializable {
 
     @FXML
     public Text usdField;
@@ -29,7 +33,6 @@ public class CalculatorExchangePageController {
     public Text gbpField;
     @FXML
     public Text chfField;
-
     @FXML
     public TextField amount;
 
@@ -42,7 +45,7 @@ public class CalculatorExchangePageController {
     @FXML
     public void reloadChart(ActionEvent event) throws IOException {
 
-        calculatorTitle.setText("Kurs z dnia" + date.getValue().toString());
+        calculatorTitle.setText("Kurs z dnia " + date.getValue().toString());
         CurrencyHolder holder = new CurrencyHolder();
 
         String inputText = amount.getText();
@@ -51,12 +54,21 @@ public class CalculatorExchangePageController {
             System.out.println("Parsed double value: " + doubleValue);
             holder.UpdateCurrencyValues(doubleValue, date.getValue(), this);
         } catch (NumberFormatException e) {
-            System.out.println("Invalid input. Please enter a valid double value.");
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
+            System.out.println("Nieprawidłowa wartość do przewalutowania");
+        } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.date.setValue(LocalDate.now().minusDays(4));
+        this.usdField.setText("");
+        this.eurField.setText("");
+        this.gbpField.setText("");
+        this.chfField.setText("");
+        this.amount.setText("100");
+        this.calculatorTitle.setText("Kurs z dnia " + date.getValue().toString());
     }
 
     @FXML
